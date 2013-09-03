119a3f4a2f7a523a1cd524f503a5158c14da6348
Lars Toft Jacobsen <lars@boxed.dk>
2013-09-03 20:07:09 +0200

    last snapshot before simplification overhaul

--- zumobot.ino
+++ zumobot.ino
@@ -1,14 +1,13 @@
 #include <ChibiOS_AVR.h>
-#include <chevents.h>
 #include <ZumoMotors.h>
 #include <ZumoBuzzer.h>
 #include <Pushbutton.h>
 #include <Wire.h>
 #include <LSM303.h>
 
-#define SPEED_FWD       300
+#define SPEED_FWD       200
 #define SPEED_BACK      200
-#define SPEED_TURN      150
+#define SPEED_TURN      50
 
 #define CALIBRATION_SAMPLES 70  // Number of compass readings to take when calibrating
 #define CRA_REG_M_220HZ 0x1C    // CRA_REG_M value for magnetometer 220 Hz update rate
@@ -17,10 +16,10 @@
 #define IRF_SCALE_FACTOR 27 // 27 V*cm / Vout V if 
 #define IRF_VTOCM(raw) (IRF_SCALE_FACTOR / (5.0/1024*raw)) // raw analog val to cm
 
-#define SWEEP_ANGLE 60 // sweep ~30 degrees to each side on collision/proximity
+#define TURN_ANGLE 80 // sweep ~30 degrees to each side on collision/proximity
 
 // state enum
-enum {STOP, GO, DRIVE, TURN, SWEEP_RIGHT, SWEEP_LEFT};
+enum {STOP, GO, DRIVE, TURN};
 
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
@@ -39,30 +38,30 @@ volatile int state;
 // Distance data
 volatile uint32_t distance;
 
-// Collision bit
-volatile boolean collision;
-
 // Heading data
-volatile int orientation;
+volatile int heading;
 volatile int target;
 
 //------------------------------------------------------------------------------
 // Events
-// EVENTSOURCE_DECL(esCollision);
+EVENTSOURCE_DECL(esDistance);
+EVENTSOURCE_DECL(esHeading);
+EventListener elOnDistance;
+EventListener elOnHeading;
 
 //------------------------------------------------------------------------------
 // Distance sensor thread
 
-static WORKING_AREA(waThread1, 64);
+static WORKING_AREA(waThreadRange, 64);
 
-static msg_t Thread1 (void *arg) {
+static msg_t ThreadRange (void *arg) {
 
   const uint8_t SENSOR_PIN = A5;
-  // Read data every 40 ms.
+  // Read data every 50 ms.
   systime_t wakeTime = chTimeNow();
 
   while (1) {
-    // Add ticks for 40 ms.
+    // Add ticks for 50 ms.
     wakeTime += MS2ST(50);
     chThdSleepUntil(wakeTime);
 
@@ -72,43 +71,47 @@ static msg_t Thread1 (void *arg) {
     chMtxLock(&dataMutex);
     distance = sensorVal;
     chMtxUnlock();
+
+    if(sensorVal > 500)
+      chEvtBroadcastFlags(&esDistance, EVENT_MASK(1));
+    
+    Serial.println("DISTANCE thread round completed");
   }
 }
 
 //------------------------------------------------------------------------------
 // Accelerometer thread
 
-static WORKING_AREA(waThread2, 64);
+// static WORKING_AREA(waThreadAccel, 64);
 
-static msg_t Thread2 (void *arg) {
+// static msg_t ThreadAccel (void *arg) {
 
-  // Read data every 15 ms.
-  systime_t wakeTime = chTimeNow();
+//   // Read data every 15 ms.
+//   systime_t wakeTime = chTimeNow();
 
-  while (1) {
-    // Add ticks for 15 ms.
-    wakeTime += MS2ST(15);
-    chThdSleepUntil(wakeTime);
+//   while (1) {
+//     // Add ticks for 15 ms.
+//     wakeTime += MS2ST(15);
+//     chThdSleepUntil(wakeTime);
 
-    // read INT1
-    byte col = compass.readAccReg(LSM303_INT1_SRC_A);
-
-    // write collision bit if necessary
-    chMtxLock(&dataMutex);
-    if(state == DRIVE) collision = col>>6 & 0x01;
-    chMtxUnlock();
-
-  }
-}
+//     // read INT1
+//     byte col = compass.readAccReg(LSM303_INT1_SRC_A);
+    
+//     if(col>>6 & 0x01)
+//       chEvtBroadcastFlags(&esCollision, EVENT_MASK(3));
+//   }
+// }
 
 //------------------------------------------------------------------------------
 // Compass thread
 
-static WORKING_AREA(waThread3, 128);
+static WORKING_AREA(waThreadCompass, 128);
 
-static msg_t Thread3 (void *arg) {
+static msg_t ThreadCompass (void *arg) {
 
   while (1) {
+
+    long t1 = millis();
 
     LSM303::vector v = {0, 0, 0};
     // average of 10 readings
@@ -129,8 +132,11 @@ static msg_t Thread3 (void *arg) {
 
     // write heading
     chMtxLock(&dataMutex);
-    orientation = angle;
+    heading = angle;
     chMtxUnlock();
+
+    Serial.print("HEADING thread round completed: ");
+    Serial.println(millis() - t1);
 
     chThdSleepMilliseconds(20);
   }
@@ -139,33 +145,28 @@ static msg_t Thread3 (void *arg) {
 //------------------------------------------------------------------------------
 // Motor thread
 
-static WORKING_AREA(waThread4, 128);
+static WORKING_AREA(waThreadMotor, 128);
 
-static msg_t Thread4 (void *arg) {
+static msg_t ThreadMotor (void *arg) {
 
-  int l_head = 0;
-  int l_dist = 0;
+  // register events
+  chEvtRegister(&esDistance, &elOnDistance, EVENT_MASK(1));
+  chEvtRegister(&esHeading, &elOnHeading, EVENT_MASK(2));
 
   while (1) {
 
     // Read shared data.
     chMtxLock(&dataMutex);
 
-    boolean col = collision;
     int dist = distance;
     int tmpstate = state;
-    int head = orientation;
+    int head = heading;
     int tmptarget = target;
 
     chMtxUnlock();
 
     // get relative heading
     int relative_heading = relativeHeading(head, tmptarget);
-    // get longest distance readings
-    if(dist > l_dist) {
-      l_dist = dist;
-      l_head = head;
-    }
 
     // ready to go
     if(tmpstate == GO) {
@@ -180,16 +181,11 @@ static msg_t Thread4 (void *arg) {
       motors.setSpeeds(0, 0);
     }
     // closing in: decelerate
-    else if(tmpstate == DRIVE && (dist >= 300 && dist <= 500)) {
-      int newSpeed = map(dist, 300, 500, SPEED_FWD, 100);
-      motors.setSpeeds(newSpeed, newSpeed);
-    }
-    // COLLISION or proximity: turn
-    else if(tmpstate == DRIVE && (dist > 500 || col)) {
-      // reset ldist
-      l_dist = 0;
-      l_head = 0;
-
+    else if(tmpstate == DRIVE) {
+      Serial.println("waiting for collision");
+      chEvtWaitOne(EVENT_MASK(1)); // collision or proximity
+      Serial.println("got it");
+      
       // back up
       motors.setSpeeds(0, 0);
       chThdSleepMilliseconds(100);
@@ -198,43 +194,12 @@ static msg_t Thread4 (void *arg) {
 
       // sweep right
       chMtxLock(&dataMutex);
-      collision = false; // better clear this
-      state = SWEEP_RIGHT;
-      target = (head + SWEEP_ANGLE) % 360; // set right target angle
+      state = TURN;
+      target = (head + TURN_ANGLE) % 360; // set right target angle
       chMtxUnlock();
     }
-    else if(tmpstate == SWEEP_RIGHT) {
-      // if reached destination within threshold stop and sweep left else continue right sweep
-      if(abs(relative_heading) < DEVIATION_THRESHOLD) {
-        motors.setSpeeds(0, 0);
-        chThdSleepMilliseconds(100);
-        chMtxLock(&dataMutex);
-        target = (head - 2*SWEEP_ANGLE) % 360; // set right target angle
-        state = SWEEP_LEFT;
-        chMtxUnlock();
-      }
-      else {
-        int speed = map(abs(relative_heading), 0, 180, SPEED_TURN, 70);
-        motors.setSpeeds(speed, -speed);
-      }
-    }
-    else if(tmpstate == SWEEP_LEFT) {
-      // if reached destination within threshold stop and sweep left else continue right sweep
-      if(abs(relative_heading) < DEVIATION_THRESHOLD) {
-        motors.setSpeeds(0, 0);
-        chThdSleepMilliseconds(100);
-        chMtxLock(&dataMutex);
-        //target = (head + 2*SWEEP_ANGLE) % 360; // set longest distance heading as target
-        target = l_head;
-        state = TURN;
-        chMtxUnlock();
-      }
-      else {
-        int speed = map(abs(relative_heading), 0, 180, SPEED_TURN, 90);
-        motors.setSpeeds(-speed, speed);
-      }
-    }
     else if(tmpstate == TURN) {
+      chEvtWaitOne(EVENT_MASK(2));
       // if reached destination within threshold stop and sweep left else continue right sweep
       if(abs(relative_heading) < DEVIATION_THRESHOLD) {
         motors.setSpeeds(0, 0);
@@ -244,15 +209,11 @@ static msg_t Thread4 (void *arg) {
         chMtxUnlock();
       }
       else {
-        int speed = map(abs(relative_heading), 0, 180, SPEED_TURN, 90);
-        if(relative_heading < 0) {
-          motors.setSpeeds(-speed, speed);
-        }
-        else {
-          motors.setSpeeds(speed, -speed);
-        }
+        int speed = (SPEED_FWD*abs(relative_heading)/180) + SPEED_TURN;
+        motors.setSpeeds(speed, -speed);
       }
     }
+    
 
     chThdSleepMilliseconds(20);
 
@@ -287,35 +248,35 @@ void setup() {
   float max_x_avg[CALIBRATION_SAMPLES];
   float max_y_avg[CALIBRATION_SAMPLES];
   
-  button.waitForButton();
+  // button.waitForButton();
   
-  // To calibrate the magnetometer, the Zumo spins to find the max/min
-  // magnetic vectors. This information is used to correct for offsets
-  // in the magnetometer data.
-  motors.setLeftSpeed(200);
-  motors.setRightSpeed(-200);
+  // // To calibrate the magnetometer, the Zumo spins to find the max/min
+  // // magnetic vectors. This information is used to correct for offsets
+  // // in the magnetometer data.
+  // motors.setLeftSpeed(200);
+  // motors.setRightSpeed(-200);
 
-  for(index = 0; index < CALIBRATION_SAMPLES; index ++) {
-    // Take a reading of the magnetic vector and store it in compass.m
-    compass.read();
+  // for(index = 0; index < CALIBRATION_SAMPLES; index ++) {
+  //   // Take a reading of the magnetic vector and store it in compass.m
+  //   compass.read();
 
-    running_min.x = min(running_min.x, compass.m.x);
-    running_min.y = min(running_min.y, compass.m.y);
+  //   running_min.x = min(running_min.x, compass.m.x);
+  //   running_min.y = min(running_min.y, compass.m.y);
 
-    running_max.x = max(running_max.x, compass.m.x);
-    running_max.y = max(running_max.y, compass.m.y);
+  //   running_max.x = max(running_max.x, compass.m.x);
+  //   running_max.y = max(running_max.y, compass.m.y);
 
-    delay(50);
-  }
+  //   delay(50);
+  // }
 
-  motors.setLeftSpeed(0);
-  motors.setRightSpeed(0);
+  // motors.setLeftSpeed(0);
+  // motors.setRightSpeed(0);
 
-  // Set calibrated values to compass.m_max and compass.m_min
-  compass.m_max.x = running_max.x;
-  compass.m_max.y = running_max.y;
-  compass.m_min.x = running_min.x;
-  compass.m_min.y = running_min.y;
+  // // Set calibrated values to compass.m_max and compass.m_min
+  // compass.m_max.x = running_max.x;
+  // compass.m_max.y = running_max.y;
+  // compass.m_min.x = running_min.x;
+  // compass.m_min.y = running_min.y;
 
   // set default state
   state = GO;
@@ -342,31 +303,31 @@ void setup() {
 void mainThread() {
 
   // start distance thread
-  chThdCreateStatic(waThread1, sizeof(waThread1),
-  NORMALPRIO + 1, Thread1, NULL);
+  chThdCreateStatic(waThreadRange, sizeof(waThreadRange),
+  NORMALPRIO + 2, ThreadRange, NULL);
 
-  // start accelerometer thread
-  chThdCreateStatic(waThread2, sizeof(waThread2),
-  NORMALPRIO + 1, Thread2, NULL);
+  // // start accelerometer thread
+  // chThdCreateStatic(waThreadAccel, sizeof(waThreadAccel),
+  // NORMALPRIO + 1, ThreadAccel, NULL);
   
   // start compass thread
-  chThdCreateStatic(waThread3, sizeof(waThread3),
-  NORMALPRIO + 2, Thread3, NULL);
+  chThdCreateStatic(waThreadCompass, sizeof(waThreadCompass),
+  NORMALPRIO + 1, ThreadCompass, NULL);
 
   // start motor thread
-  chThdCreateStatic(waThread4, sizeof(waThread4),
-  NORMALPRIO + 3, Thread4, NULL);
+  chThdCreateStatic(waThreadMotor, sizeof(waThreadMotor),
+  NORMALPRIO + 3, ThreadMotor, NULL);
 
 }
 
 //------------------------------------------------------------------------------
 void loop() {
-
+  // not used
 }
 
 //------------------------------------------------------------------------------
 // Yields the angle difference in degrees between two headings
-int relativeHeading(int heading_from, int heading_to) {
+static int relativeHeading(int heading_from, int heading_to) {
   int relative_heading = heading_to - heading_from;
 
   // constrain to -180 to 180 degree range
@@ -377,5 +338,6 @@ int relativeHeading(int heading_from, int heading_to) {
 
   return relative_heading;
 }
+
 
 
